e7d01912f2a767731348e262d41e7163a0b8903e
derek-cornelison <dcornelison1216@gmail.com>
2020-05-13 00:16:08 -0400

    Step 2 - useDarkMode completed

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -1,5 +1,16 @@
 import { useLocalStorage } from './useLocalStorage';
+import { useEffect } from 'react';
 
-export const useDarkMode = () => {
-  
+export const useDarkMode = (key, initialState) => {
+  const [isDark, setIsDark] = useLocalStorage(key, initialState);
+
+  useEffect(() => {
+    if(isDark) {
+      document.body.classList.add('dark-mode')
+    } else if(!isDark) {
+      document.body.classList.remove('dark-mode')
+    }
+  }, darkMode);
+
+  return [isDark, setIsDark];
 };
